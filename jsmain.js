d155b22e2ce5898a4a8e4505ddf01068b2ed0a2b
S VanDam <stephen.vandam@flemingcollege.ca>
2023-05-18 14:28:05 -0400

    added the current website

---
+++ jsmain.js
@@ -0,0 +1,10 @@
+//  Javascript function for navbar and responsive design 
+
+function navFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
