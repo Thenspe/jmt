681dc87f4aed68671dade29f404e7bd6d15e2815
Thenspe <91274102+Thenspe@users.noreply.github.com>
2024-05-23 14:46:44 -0400

    Some spacing changes

--- jsmain.js
+++ jsmain.js
@@ -17,7 +17,7 @@ window.onscroll = function() {
 function navFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-        x.className += " responsive";
+        x.className += "responsive";
     } else {
         x.className = "topnav";
     }
