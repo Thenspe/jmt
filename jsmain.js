48e36c5dff150d75b5fa29ed2d53822d8d4564be
Thenspe <91274102+Thenspe@users.noreply.github.com>
2024-06-05 22:22:23 -0400

    @media update

--- jsmain.js
+++ jsmain.js
@@ -1,18 +1,3 @@
-//Function to show/hide the header.
-//when user scrolls down, hide navbar. when user scrolls up, show navbar
-var prevScrollpos = window.scrollY;
-window.onscroll = function() {
-    var currentScrollPos = window.scrollY;
-    if (prevScrollpos > currentScrollPos) {
-        //scrolled up, show header
-        document.getElementById("myhead").style.display = "block";
-    } else {
-        //scrolled down, hide header
-        document.getElementById("myhead").style.display = "none";
-    }
-    prevScrollpos = currentScrollPos;
-}
-
 //  Javascript function for navbar and responsive design 
 function navFunction() {
     var x = document.getElementById("myTopnav");
