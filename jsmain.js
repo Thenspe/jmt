e25ddc30cb8ae5f0990808a6f459820250d1bfc0
Thenspe <91274102+Thenspe@users.noreply.github.com>
2024-06-05 23:19:40 -0400

    Fixed the flippy boxes on mobile, and made them easy to update

--- jsmain.js
+++ jsmain.js
@@ -7,3 +7,19 @@ function navFunction() {
         x.className = "topnav";
     }
 }
+console.log("Does this work?");
+
+const para = document.createElement("h3");
+para.innerHTML = "There is other content going here.";
+document.getElementById("notflippy").appendChild(para);
+
+const nodes = ["pro1","pro2","pro3","pro4","pro5","pro6","pro7","pro8"];
+const clones = ["proA","proB","proC","proD","proE","proF","proG","proH"];
+for(let i in nodes) {
+    const node = document.getElementById(nodes[i]);
+    const clone = node.cloneNode(true);
+    document.getElementById(clones[i]).appendChild(clone);
+    console.log(nodes[i]);
+    console.log(clones[i]);
+    console.log(clone);
+}
